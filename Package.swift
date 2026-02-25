8acdd6a7a8faa4bf43ef8818d3f783d8c2a26855
Dr. Mikholae Hutchinson <admin@swiftpython.dev>
2026-02-25 12:15:01 -0500

    Use local xcframework path (no https required)

--- Package.swift
+++ Package.swift
@@ -10,8 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SwiftPythonRuntime",
-            url: "file:///Users/mikhutchinson/CascadeProjects/SwiftPython/.build/framework/SwiftPythonRuntime.zip",
-            checksum: "dae6b1299e26f214d95f26cdabc100cf49dbfbe18ab8c3b3ddb8ca028a7e1e0d"
+            path: "SwiftPythonRuntime.xcframework"
         ),
     ]
 )
